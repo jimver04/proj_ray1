9ba93b2d1ea3e33a696f0f89400978e7bfb86c4a
Youbing Zhao <zhaoyoubing@gmail.com>
2026-02-15 22:34:36 +0000

    added vec3 near_zero()

--- src/vec3.h
+++ src/vec3.h
@@ -45,6 +45,15 @@ class vec3 {
         return e[0]*e[0] + e[1]*e[1] + e[2]*e[2];
     }
 
+    // >>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>
+    bool near_zero() const {
+        // Return true if the vector is close to zero in all dimensions.
+        auto s = 1e-8;
+        return (std::fabs(e[0]) < s) && (std::fabs(e[1]) < s) && (std::fabs(e[2]) < s);
+    }
+    // <<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<
+
+
     static vec3 random() {
         return vec3(random_double(), random_double(), random_double());
     }
@@ -52,6 +61,8 @@ class vec3 {
     static vec3 random(double min, double max) {
         return vec3(random_double(min,max), random_double(min,max), random_double(min,max));
     }
+
+
 };
 
 // point3 is just an alias for vec3, but useful for geometric clarity in the code.
