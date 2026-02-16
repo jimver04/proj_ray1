a3cd3ca2708872ad3ae926d78c13cb6801f3f4bb
Youbing Zhao <zhaoyoubing@gmail.com>
2026-02-16 11:58:26 +0000

    total reflection and schlick added

--- src/material.h
+++ src/material.h
@@ -53,12 +53,20 @@ class metal : public material {
     }
 };
 
-//>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>
 class dielectric : public material {
   private:
     // Refractive index in vacuum or air, or the ratio of the material's refractive index over
     // the refractive index of the enclosing media
     double refraction_index;
+
+    //>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>
+    static double reflectance(double cosine, double refraction_index) {
+        // Use Schlick's approximation for reflectance.
+        auto r0 = (1 - refraction_index) / (1 + refraction_index);
+        r0 = r0*r0;
+        return r0 + (1-r0)*std::pow((1 - cosine),5);
+    }
+    //<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<
 
   public:
     dielectric(double refraction_index) : refraction_index(refraction_index) {}
@@ -71,11 +79,30 @@ class dielectric : public material {
         vec3 unit_direction = unit_vector(r_in.direction());
         vec3 refracted = refract(unit_direction, rec.normal, ri);
 
-        scattered = ray(rec.p, refracted);
+        // >>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>
+        double cos_theta = std::fmin(dot(-unit_direction, rec.normal), 1.0);
+        double sin_theta = std::sqrt(1.0 - cos_theta*cos_theta);
+
+        bool cannot_refract = ri * sin_theta > 1.0;
+        vec3 direction;
+
+        if (cannot_refract)
+            direction = reflect(unit_direction, rec.normal);
+        else
+            direction = refract(unit_direction, rec.normal, ri);
+
+        if (cannot_refract || reflectance(cos_theta, ri) > random_double())
+            direction = reflect(unit_direction, rec.normal);
+        else
+            direction = refract(unit_direction, rec.normal, ri);
+        
+        scattered = ray(rec.p, direction);
+        // <<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<
+
+        
         return true;
     }
 };
-//<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<
 
 
 #endif
